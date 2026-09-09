050139dcc6b0b9ba53a25d8d7f22162054017458
Алексей <aleksey1danilov@gmail.com>
2026-09-09 23:49:39 +0300

    Fix database for PostgreSQL

--- app/core/database.py
+++ app/core/database.py
@@ -9,9 +9,10 @@ load_dotenv()
 # Используем SQLite для простоты (потом можно заменить на PostgreSQL)
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./learning.db")
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
-)
+if "postgresql" in DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+else:
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
